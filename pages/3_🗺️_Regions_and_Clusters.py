53a7364a231397e0363f3fc23dac9e916323c154
queby <nathanyaqueby21@gmail.com>
2023-04-29 16:26:13 +0200

    add regions to streamlit

--- pages/3_🗺️_Regions_and_Clusters.py
+++ pages/3_🗺️_Regions_and_Clusters.py
@@ -51,16 +51,18 @@ st.sidebar.markdown("---")
 st.sidebar.markdown("Read more about the project on [GitHub](www.github.com/nathanyaqueby/too-good-to-code).")
 
 m = leafmap.Map(center=[40, -100], zoom=4)
-cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
-regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
+cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/world_cities.csv'
+regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/world_cities.geojson'
 
 m.add_geojson(regions, layer_name='US Regions')
 m.add_points_from_xy(
     cities,
     x="longitude",
     y="latitude",
-    color_column='region',
-    icon_names=['gear', 'map', 'leaf', 'globe'],
+    # color_column='region',
+    # icon_names=['gear', 'map', 'leaf', 'globe'],
     spin=True,
     add_legend=True,
 )
+
+m.to_streamlit(height=700)
