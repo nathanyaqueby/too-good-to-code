61b17f3521ea413cd179223659572ca68e764269
queby <nathanyaqueby21@gmail.com>
2023-04-30 03:00:28 +0200

    round lat and long

--- Home.py
+++ Home.py
@@ -154,10 +154,10 @@ if loc:
     #             ''')
     
     col2.markdown("### Details")
-    # get the solar_area, energy_produced, and radiance based on the latitude and longitude
-    solar_area = df.loc[(df["center_lat"] == lat) & (df["center_long"] == lon), "solar_area"].values[0]
-    energy_produced = df.loc[(df["center_lat"] == lat) & (df["center_long"] == lon), "energy_produced"].values[0]
-    radiance = df.loc[(df["center_lat"] == lat) & (df["center_long"] == lon), "radiance"].values[0]
+    # get the solar_area, energy_produced, and radiance based on the closest latitude and longitude
+    solar_area = df.loc[(df["center_lat"].round(3) == lat.round(3)) & (df["center_long"].round(3) == lon.round(3)), "solar_area"].values[0]
+    energy_produced = df.loc[(df["center_lat"].round(3) == lat.round(3)) & (df["center_long"].round(3) == lon.round(3)), "energy_produced"].values[0]
+    radiance = df.loc[(df["center_lat"].round(3) == lat.round(3)) & (df["center_long"].round(3) == lon.round(3)), "radiance"].values[0]
 
     # list the location, area (m^2), sun radiation (kwh/m^2), solar efficiency (0-10), and amount of electric potential (kwh)
     col2.markdown("📍 Location: "+str(location.address))
