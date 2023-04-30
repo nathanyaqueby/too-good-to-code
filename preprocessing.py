5cedeecdc2a4c9be67c5f38b12fb520d8d6022c0
queby <nathanyaqueby21@gmail.com>
2023-04-30 03:09:24 +0200

    debug image

--- preprocessing.py
+++ preprocessing.py
@@ -5,8 +5,6 @@ def save_satellite_image(lat, long, file='pic.jpg', zoom=20):
     # Enter your api key here
     api_key = st.secrets["API_TOKEN"]
 
-    st.write(api_key)
-
     # url variable store url
     url = "https://maps.googleapis.com/maps/api/staticmap?"
 
@@ -22,8 +20,6 @@ def save_satellite_image(lat, long, file='pic.jpg', zoom=20):
     # r.content gives content,
     # in this case gives image
     f.write(r.content)
-
-    f.close()
 
     st.image(file, use_column_width=True)
 
